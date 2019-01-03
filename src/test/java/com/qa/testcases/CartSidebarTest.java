077d5f344300052bd5ff48940b3e47942beeda66
Mustafa Bacchus <37421260+mustafabacchus@users.noreply.github.com>
2019-01-03 12:02:16 -0500

    Completed sidebar sync test with cart

--- src/test/java/com/qa/testcases/CartSidebarTest.java
+++ src/test/java/com/qa/testcases/CartSidebarTest.java
@@ -1,6 +1,7 @@
 package com.qa.testcases;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import java.util.List;
 
@@ -38,42 +39,96 @@ public class CartSidebarTest extends Base {
 	
 	
 	@Test(priority=0)
-	public void cartSyncWithSidebar() {
+	public void sidebarSyncWithCart() {
+		util.displayTestCaseTitle("Sidebar Sync With Cart");
+		boolean itemsInCart = false;
+		
 		//Add items to the cart
 		for (int i = 0; i < landingPage.getSpecialOffers().size(); i++) {
 			List<WebElement> products = landingPage.getSpecialOffers();
 			landingPage.addItemToCart(products.get(i));
 			
-			int totalQty = 0;
 			boolean ok = true;
+			int cartItems = 0;
 			try {
+				//Confirm item is added to the cart
 				cartPage.validateTitle();
-				WebElement lastItem = cartPage.getItemsInCart().get(cartPage.getItemsInCart().size()-1);
-				totalQty = totalQty + cartPage.getQuantity(lastItem);
 				ok = true;
-			} catch (java.lang.AssertionError e) {
-				driver.navigate().to(prop.getProperty("url"));
-				ok = false;
-			}
-			//Check sync
-			if (ok) {
-				assertEquals(totalQty, cartSidebar.getNumItemsInCart());
+				itemsInCart = true;
+				List<WebElement> items = cartPage.getItemsInCart();
+				
+				//Display added item
+				System.out.println(cartPage.getItemName(items.get(items.size()-1)) + 
+						" +" + Integer.toString(cartPage.getQuantity(items.get(items.size()-1))));
+				
+				//Update quantity from last added item
+				for (WebElement item: items) {
+					cartItems += cartPage.getQuantity(item);
+				}
+				
 				cartPage.clickContinueShopping();
+			} catch (java.lang.AssertionError e) {
+				ok = false;
+				driver.navigate().to(prop.getProperty("url"));
 			}
-		}
-		/*
-		//Move to cart page
-		cartSidebar.clickViewCart();
-		if (cartPage.getItemsInCart().size() != 0) {
-			//Delete each item from cart
-			int total = cartPage.getItemsInCart().size();
-			for (int i = 0; i < total; i++) {
-				cartPage.clickTrashIcon(cartPage.getItemsInCart().get(0));
+			
+			if (ok) {
 				//Check sync
-				assertEquals(cartPage.getItemsInCart().size(), cartSidebar.getNumItemsInCart());
+				assertEquals(cartSidebar.getNumItemsInCart(), cartItems);
+				//Display results
+				System.out.println("Cart Sidebar: " + Integer.toString(cartSidebar.getNumItemsInCart()) + 
+						"; Items In Cart: " + Integer.toString(cartItems));
+				System.out.println();
 			}
 		}
-		*/
+		
+		
+		//Remove items from the cart
+		cartSidebar.clickViewCart();
+		if (itemsInCart) {
+			for (int i = cartPage.getItemsInCart().size()-1; i >= 0; i--) {
+				List<WebElement> items = cartPage.getItemsInCart();
+				
+				//Display removed item
+				System.out.println(cartPage.getItemName(items.get(0)) + 
+						" -" + Integer.toString(cartPage.getQuantity(items.get(0))));
+				//Remove item
+				cartPage.clickTrashIcon(items.get(0));
+				
+				//Update quantity from removed item
+				int cartItems = 0;
+				if (i > 0) {
+					items = cartPage.getItemsInCart();
+					for (WebElement item: items) {
+						cartItems += cartPage.getQuantity(item);
+					}
+				}
+				
+				//Check sync
+				if (i > 0) {
+					assertEquals(cartSidebar.getNumItemsInCart(), cartItems);
+					
+					//Display results
+					System.out.println("Cart Sidebar: " + Integer.toString(cartSidebar.getNumItemsInCart()) + 
+							"; Items In Cart: " + Integer.toString(cartItems));
+					System.out.println();
+				} else {
+					assertEquals(cartSidebar.getNumItemsInCart(), 0);
+					cartPage.validateEmptyCartMessage();
+					
+					//Display results
+					System.out.println("Cart Sidebar: " + Integer.toString(cartSidebar.getNumItemsInCart()) +
+							"; Empty cart message validated.");
+				}
+			}
+		} else {
+			System.out.println("No items were added to the cart.");
+			assertEquals(cartSidebar.getNumItemsInCart(), 0);
+			cartPage.validateEmptyCartMessage();
+			
+			System.out.println("Cart Sidebar: " + Integer.toString(cartSidebar.getNumItemsInCart()) +
+					"; Empty cart message validated.");
+		}
 	}
 	
 	
